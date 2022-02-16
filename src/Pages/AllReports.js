08ed6db08c07d16cf2cb53e4c2096b838822f668
CrewManny10 <92958041+CrewManny10@users.noreply.github.com>
2022-02-16 18:41:40 -0500

    shsh

--- src/Pages/AllReports.js
+++ src/Pages/AllReports.js
@@ -1,63 +1,75 @@
+import { render } from "@testing-library/react";
 import React from "react";
+
+//import ArrayImageData from '../AllReports/CardData';
+//import CardData from '../AllReports/CardData';
 
 import '../PagesStyles/AllReports.style.css';
 
 function AllReports() {
 
-  const images =[
-    "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
-    "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
-    "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
-    "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
-    "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg"
-  ]
-  const patients = [
-    "Amanda Lastname",
-    "Jenniffer Lastname",
-    "Betty Lastname",
-    "Karl Lastname",
-    "Matthew Lastname"
-  ]
+    const images =[
+      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
+      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
+      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
+      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
+      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
+      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
+    ];
 
-  return (
-    <div className="AllReports-wrapper">
-        <div className="AllReports-row">
-          <div className="AllReports-column">
-            <p className="AllReports-p"> {patients[0]} </p>
-            <img className="AllReports-img" src={ images[0] } alt="Patient's lungs"/>
+    const patients = [
+      'Amanda Lastname',
+      'Jenniffer Lastname',
+      'Betty Lastname',
+      'Karl Lastname',
+      'Matthew Lastname',
+      'tess Last name'
+    ]
+  
+  render();{
+    return (
+      <div className="AllReports-wrapper">
+          <div className="AllReports-row">
+            <div className="AllReports-column">
+              <p className="AllReports-p"> {patients[0]} </p>
+              <img className="AllReports-img" src={ images[0] } alt="Patient's lungs"/>
           </div>
 
-          <div className="AllReports-column">
-            <p className="AllReports-p"> {patients[1]} </p>
-            <img className="AllReports-img" src={ images[1] } alt="Patient's lungs"/>
-          </div>
+            <div className="AllReports-column">
+              <p className="AllReports-p"> {patients[1]} </p>
+              <img className="AllReports-img" src={ images[1] } alt="Patient's lungs"/>
+            </div>
 
-          <div className="AllReports-column">
-            <p className="AllReports-p"> {patients[2]} </p>
-            <img className="AllReports-img" src={ images[2] } alt="Patient's lungs"/>
-          </div>
+            <div className="AllReports-column">
+              <p className="AllReports-p"> {patients[2]} </p>
+              <img className="AllReports-img" src={ images[2] } alt="Patient's lungs"/>
+            </div>
 
-          <div className="AllReports-column">
-            <p className="AllReports-p"> {patients[3]} </p>
-            <img className="AllReports-img" src={ images[3] } alt="Patient's lungs"/>
-          </div>
+            <div className="AllReports-column">
+              <p className="AllReports-p"> {patients[3]} </p>
+              <img className="AllReports-img" src={ images[3] } alt="Patient's lungs"/>
+            </div>
 
-          <div className="AllReports-column">
-            <p className="AllReports-p"> {patients[4]} </p>
-            <img className="AllReports-img" src={ images[4] } alt="Patient's lungs"/>
-          </div>
+            <div className="AllReports-column">
+              <p className="AllReports-p"> {patients[4]} </p>
+              <img className="AllReports-img" src={ images[4] } alt="Patient's lungs"/>
+            </div>
 
-          <div className="AllReports-column">
-            <p className="AllReports-p"> {patients[0]} </p>
-            <img className="AllReports-img" src={ images[0] } alt="Patient's lungs"/>
+            <div className="AllReports-column">
+              <p className="AllReports-p"> {patients[5]} </p>
+              <img className="AllReports-img" src={ images[5] } alt="Patient's lungs"/>
+            </div>
+      
           </div>
-          
+        <div className="AllReports-sorting">
+          <p>Filter</p>
         </div>
-      <div className="AllReports-sorting">
-        <p>SORTING TOOL GOES HERE</p>
+    
+    
       </div>
-    </div>
-  );
-}
-
-export default AllReports;
+    
+    );
+    }
+  
+  }
+  export default AllReports;

cf27eaed3d058608c21f9ecb977370ee4c88cd2d
CrewManny10 <92958041+CrewManny10@users.noreply.github.com>
2022-02-20 14:18:27 -0500

    new

---
+++ src/IMAGES.js
@@ -0,0 +1,27 @@
+
+    const images =[{
+      id: 1,
+      name: "Amanda",
+      src: "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg"
+    },
+
+    {
+          id: 2,
+          name: "Amanda",
+          src: "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg"
+     },
+
+     {
+        id: 3,
+        name: "Amanda",
+        src: "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg"
+     },
+
+     {
+        id: 4,
+        name: "Amanda",
+        src: "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg"
+     }
+    ]
+
+export default images;
--- src/Pages/AllReports.js
+++ src/Pages/AllReports.js
@@ -1,72 +1,31 @@
 import { render } from "@testing-library/react";
-import React from "react";
 
-//import ArrayImageData from '../AllReports/CardData';
-//import CardData from '../AllReports/CardData';
+import images from '../IMAGES';
+
 
 import '../PagesStyles/AllReports.style.css';
 
+
 function AllReports() {
-
-    const images =[
-      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
-      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
-      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
-      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
-      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
-      "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-1591061.jpg&fm=jpg",
-    ];
-
-    const patients = [
-      'Amanda Lastname',
-      'Jenniffer Lastname',
-      'Betty Lastname',
-      'Karl Lastname',
-      'Matthew Lastname',
-      'tess Last name'
-    ]
   
   render();{
     return (
       <div className="AllReports-wrapper">
           <div className="AllReports-row">
+            
             <div className="AllReports-column">
-              <p className="AllReports-p"> {patients[0]} </p>
-              <img className="AllReports-img" src={ images[0] } alt="Patient's lungs"/>
-          </div>
-
-            <div className="AllReports-column">
-              <p className="AllReports-p"> {patients[1]} </p>
-              <img className="AllReports-img" src={ images[1] } alt="Patient's lungs"/>
-            </div>
-
-            <div className="AllReports-column">
-              <p className="AllReports-p"> {patients[2]} </p>
-              <img className="AllReports-img" src={ images[2] } alt="Patient's lungs"/>
-            </div>
-
-            <div className="AllReports-column">
-              <p className="AllReports-p"> {patients[3]} </p>
-              <img className="AllReports-img" src={ images[3] } alt="Patient's lungs"/>
-            </div>
-
-            <div className="AllReports-column">
-              <p className="AllReports-p"> {patients[4]} </p>
-              <img className="AllReports-img" src={ images[4] } alt="Patient's lungs"/>
-            </div>
-
-            <div className="AllReports-column">
-              <p className="AllReports-p"> {patients[5]} </p>
-              <img className="AllReports-img" src={ images[5] } alt="Patient's lungs"/>
-            </div>
-      
+              <p className="AllReports-p">{}</p>
+              {images && images.map(i => {
+                <div key={i.name} className="AllReports-row"/>
+                return <img key={i.name} className="AllReports-col" src={i.src} className="AllReports-img" alt="Patients lungs"/> })}
+              </div>
           </div>
         <div className="AllReports-sorting">
           <p>Filter</p>
         </div>
     
-    
       </div>
+    
     
     );
     }
